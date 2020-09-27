ae0d53e71d588a42e1d46f8f852767b91e87eeec
Kirill <41589091+Traineratwot@users.noreply.github.com>
2020-09-27 15:41:29 +0300

    2

--- SIH.js
+++ SIH.js
@@ -2,100 +2,100 @@
 class SIH {
 
 	constructor(selector = '', datalist = {}, hotUpdate = false) {
-		this.selector = selector;
-		this.datalist = datalist;
-		this.elem = $(selector);
+		this.selector = selector
+		this.datalist = datalist
+		this.elem = $(selector)
 		this.hotUpdate = hotUpdate
-		if (this.elem.length == 0) {
-			return false;
+		if(this.elem.length == 0) {
+			return false
 		}
-		if (this.elem.length > 1) {
+		if(this.elem.length > 1) {
 			console.warn('Input задан неоднозначно ' + selector)
-			return false;
+			return false
 		}
-		var self = this;
+		var self = this
 		this.ini()
-		this.elem.on('focus', function () {
-			self.show();
+		this.elem.on('focus', function() {
+			self.show()
 		})
-		// this.elem.on('blur', function () {
-		// 	self.hide();
-		// })
-		if (this.hotUpdate) {
-			this.elem.on('input', function () {
-				self.ini();
-			});
+		this.elem.on('blur', function() {
+			self.hide()
+		})
+		if(this.hotUpdate) {
+			this.elem.on('input', function() {
+				self.ini()
+			})
 		}
 
 	}
-
+	setDataList(datalist){
+		this.datalist = datalist
+		this.ini()
+	}
 	ini() {
-		if (this.elem.length == 0) {
-			return false;
+		if(this.elem.length == 0) {
+			return false
 		}
-		var helplist = $('<div>');
+		if(typeof this.helplist != 'undefined' && this.helplist.constructor.name == 'm') {
+			this.helplist.remove()
+		}
+
+		var helpList = $('<div>')
 		var type = Array.isArray(this.datalist)
-		for (var key in this.datalist) {
-			if (this.datalist.hasOwnProperty(key)) {
-				var element = this.datalist[key];
+		for(var key in this.datalist) {
+			if(this.datalist.hasOwnProperty(key)) {
+				var element = this.datalist[key]
 				var li = $('<li>')
 				li.attr('data-input', this.selector)
-				if (type) {
-					switch (typeof element) {
+				if(type) {
+					switch( typeof element ) {
 						case 'string':
-							var tv = element.split(":");
-							if (tv.length == 2) {
-								element = tv['1'];
-								li.text(tv[0]);
+							var tv = element.split(':')
+							if(tv.length == 2) {
+								element = tv['1']
+								li.text(tv[0])
 							} else {
-								li.text(element);
+								li.text(element)
 							}
-							break;
+							break
 						case 'date':
 
-							break;
+							break
 
 						default:
 							li.text(element)
-							break;
+							break
 					}
 				} else {
-					switch (typeof element) {
+					switch( typeof element ) {
 						case 'object':
-							switch (element.constructor.name) {
+							switch( element.constructor.name ) {
 								case 'Date':
-									li.text(element);
-									break;
+									li.text(element)
+									break
 
 								default:
-									break;
+									break
 							}
-							break;
+							break
 
 						default:
 							li.text(key)
-							break;
+							break
 					}
 				}
-				li.attr('data-value', element);
-				li.appendTo(helplist);
+				li.attr('data-value', element)
+				li.appendTo(helpList)
 
 			}
 		}
-		this.helplist = helplist;
-	}
-	show() {
-		if (this.elem.length == 0) {
-			return false;
-		}
-		if (!this.helplist) {
-			this.ini()
-		}
-		var self = this;
-		this.helplist.appendTo('body')
-		var height = parseFloat(this.elem.css('height')) + parseFloat(this.elem.css('padding-bottom')) + parseFloat(this.elem.css('border-bottom-width'));
+		this.helplist = helpList
 
-		var width = parseFloat(this.elem.css('width')) + parseFloat(this.elem.css('padding-left')) + parseFloat(this.elem.css('padding-right')) + parseFloat(this.elem.css('border-right-width')) + parseFloat(this.elem.css('border-left-width')) - 5;
+		var self = this
+		this.helplist.appendTo('body')
+		var height = parseFloat(this.elem.css('height')) + parseFloat(this.elem.css('padding-bottom')) + parseFloat(this.elem.css('border-bottom-width'))
+
+		var width = parseFloat(this.elem.css('width')) + parseFloat(this.elem.css('padding-left')) + parseFloat(this.elem.css('padding-right')) + parseFloat(this.elem.css('border-right-width')) + parseFloat(this.elem.css('border-left-width')) - 5
 		this.style = {
 			width: width + 'px',
 			collor: this.elem.css('collor'),
@@ -103,25 +103,38 @@ class SIH {
 			position: 'absolute',
 			display: 'none',
 		}
-		this.helplist.css(this.style);
-		this.helplist.addClass('datalist');
+		this.helplist.css(this.style)
+		this.helplist.addClass('datalist')
 		this.helplist.offset({
 			top: this.elem.offset().top + height,
 			left: this.elem.offset().left
-		});
-		this.helplist.fadeIn(100)
-		this.helplist.find('li').on('click', function () {
-			self.elem.val($(this).attr('data-value'));
+		})
+
+		this.helplist.find('li').on('click', function() {
+			self.elem.val($(this).attr('data-value'))
 		})
 	}
-	hide() {
-		if (this.elem.length == 0) {
-			return false;
+
+
+	show() {
+		if(this.elem.length == 0) {
+			return false
 		}
-		setTimeout(() => {
-			this.helplist.fadeOut(100);
-			this.helplist.remove();
-			this.helplist = null;
-		}, 200);
+		if(!this.helplist) {
+			this.ini()
+		}
+		if(!this.datalist.length) {
+			return false
+		}
+
+		this.helplist.fadeIn(100)
+	}
+
+
+	hide() {
+		if(this.elem.length == 0) {
+			return false
+		}
+		this.helplist.fadeOut(600)
 	}
 }
